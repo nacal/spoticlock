e7f42f81ea93d7254ef74db769d3b8d49ca82155
nkthkr <hikaru.nakata@nislab.doshisha.ac.jp>
2020-12-24 16:07:08 +0900

    add stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+    "stylelint-config-recommended-scss",
+  ],
+  plugins: [],
+  rules: {
+    "number-leading-zero": "never",
+  },
+};
